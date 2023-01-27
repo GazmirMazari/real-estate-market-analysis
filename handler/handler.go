a9fe2dee9dffa2a33144756a008d30d755a92979
Gazmir Mazari <37550822+GazmirDi@users.noreply.github.com>
2023-01-27 18:34:12 -0500

    add the to do list at swagger.yml file

--- handler/handler.go
+++ handler/handler.go
@@ -4,21 +4,12 @@ import (
 	"net/http"
 )
 
-// Hello from a snippet box
+// Home serves as an example endpoint
+// swagger:route POST / Home
 func Home(w http.ResponseWriter, r *http.Request) {
 	if r.URL.Path != "/" {
 		http.NotFound(w, r)
 		return
 	}
 	w.Write([]byte("Hello from a snippet box"))
-}
-
-func GetListingBased(w http.ResponseWriter, r *http.Request) {
-	if r.Method != http.MethodGet {
-		http.Error(w, "Method not allowed", http.StatusMethodNotAllowed)
-		return
-	}
-	w.Header().Set("Content-Type", "application/json")
-
-	w.Write([]byte("Get a specific listing"))
 }
