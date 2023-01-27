0904e04311cdb095e75634ad742128a43dc11fbd
Gazmir Mazari <37550822+GazmirDi@users.noreply.github.com>
2023-01-27 14:39:48 -0500

    add a GetListing

--- main.go
+++ main.go
@@ -33,7 +33,19 @@ import (
 //Hello from a snippet box
 
 func home(w http.ResponseWriter, r *http.Request) {
+	if r.URL.Path != "/" {
+		http.NotFound(w, r)
+		return
+	}
 	w.Write([]byte("Hello from a snippet box"))
+}
+
+func GetListing(w http.ResponseWriter, r *http.Request) {
+	if r.Method != http.MethodGet {
+		http.Error(w, "Method not allowed", http.StatusMethodNotAllowed)
+		return
+	}
+	w.Write([]byte("Get a specific listing"))
 }
 
 func main() {
